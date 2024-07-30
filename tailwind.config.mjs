d6bd51403c9fcade265245203a6f4acd0d627a04
Jack Gong <jackgong48@gmail.com>
2024-07-30 20:02:15 +1200

    INITIAL: commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        cmyk: {
+          ...require("daisyui/src/theming/themes")["cmyk"],
+          primary: "#FF0000",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
