c040bea6b17d568f9461fd7b6e626b5c468da178
songexile <psg3398@autuni.ac.nz>
2024-09-17 15:12:21 +1200

    ADDED: Middleware to handle auth on course page and course/*

--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,55 @@
-export function onRequest(context, next) {
+import { supabase } from "./lib/supabase";
+
+export async function onRequest(context, next) {
+    console.time('Middleware Execution Time'); // Start the timer
+
     // Get the URL of the incoming request
     const url = new URL(context.request.url);
     const { cookies, request } = context;
 
-
-    
-    // Retrieve tokens from cookies
-
-    
     // Check if the URL path starts with /course/
     if (url.pathname.startsWith('/course/')) {
+        // Retrieve tokens from cookies
         const accessToken = cookies.get("sb-access-token");
         const refreshToken = cookies.get("sb-refresh-token");
-        // Redirect to login if the access tokens are missing
+
+        // If tokens are missing or there's an error in token validation, redirect to login
         if (!accessToken || !refreshToken) {
-        
-            // Create a Response object that redirects to "/login"
-            return Response.redirect(new URL('/login', request.url), 302);
+            return redirectToLogin();
+        }
+
+        // Validate the tokens and set the Supabase session
+        const sessionError = await validateSession(accessToken.value, refreshToken.value);
+
+        if (sessionError) {
+            // If there is an error with the session, clear cookies and redirect to login
+            cookies.delete("sb-access-token", { path: "/" });
+            cookies.delete("sb-refresh-token", { path: "/" });
+            return redirectToLogin();
         }
     }
 
-    
-
-    // Continue to the next middleware or the final handler if the path doesn't match
+    console.timeEnd('Middleware Execution Time'); // End the timer when the middleware is done
+    // Continue to the next middleware or the final handler
     return next();
+
+    // Helper function to validate the session
+    async function validateSession(accessToken, refreshToken) {
+        try {
+            const { error: sessionError } = await supabase.auth.setSession({
+                refresh_token: refreshToken,
+                access_token: accessToken,
+            });
+            return sessionError;
+        } catch (error) {
+            console.error('Error validating session:', error);
+            return true; // Indicate an error occurred
+        }
+    }
+
+    // Helper function to handle redirects
+    function redirectToLogin() {
+        console.timeEnd('Middleware Execution Time'); // End the timer before redirecting
+        return Response.redirect(new URL('/login', request.url), 302);
+    }
 }
