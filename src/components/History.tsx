a81a469306c88eba4d478f2fab612a7a3d2e0a7f
Jack Gong <jackgong48@gmail.com>
2024-09-24 21:06:14 +1200

    FIXED: history for admin is good

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -5,9 +5,10 @@ import { format } from "date-fns";
 interface Props {
   tasks: Task[];
   timeframe?: string;
+  isAdmin?: boolean;
 }
 
-const History = ({ tasks, timeframe }: Props) => {
+const History = ({ tasks, timeframe, isAdmin }: Props) => {
   const [groupedTasks, setGroupedTasks] = useState<{ [key: string]: Task[] } | null>(null)
   // Function to group tasks by date
   const groupTasksByDate = (tasks: Task[]) => {
@@ -67,65 +68,74 @@ const History = ({ tasks, timeframe }: Props) => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-2">
-        {groupedTasks && Object.keys(groupedTasks).length > 0 ? Object.keys(groupedTasks)
-          .sort((a, b) => new Date(b).getTime() - new Date(a).getTime()) // Sort dates in descending order
-          // .filter((date) => {
-          //   const todayStart = new Date();
-          //   todayStart.setHours(0, 0, 0, 0); // Set to midnight (start of today)
-          //   // Convert the 'date' key (string) to a Date object
-          //   const taskDate = new Date(date);
-          //   taskDate.setHours(0, 0, 0, 0); // Strip time part from the taskDate
-          //   // Only return dates before today
-          //   return taskDate < todayStart;
-          // })
-          .map((date, index) => (
-            <div key={index}>
-              <div className="collapse collapse-arrow bg-black">
-                <input type="radio" name="my-accordion-1" />
-                <div className="collapse-title text-xl font-medium text-white">
-                  {format(new Date(date), "EEEE, do MMM")}
-                </div>
-                <div className="collapse-content">
-                  {groupedTasks[date]
-                    .sort(
-                      (a, b) =>
-                        new Date(b.created_at).getTime() -
-                        new Date(a.created_at).getTime()
-                    ) // Sort tasks within each date in descending order
-                    .map((task, index) => (
-                      <li
-                        key={index}
-                        className={`bg-gray-700 p-4 mb-4 w-full rounded-2xl flex items-center transition-all gap-4 ${task.is_complete
-                          ? "line-through bg-green-300/40 text-black/50"
-                          : ""
-                          }`}
-                      >
-                        <div
-                          className={`rounded-full flex justify-center items-center w-6 aspect-square ${task.is_complete ? "bg-green-500" : "bg-primary"
+        {groupedTasks && Object.keys(groupedTasks).length > 0 ? (
+          Object.keys(groupedTasks)
+            .sort((a, b) => new Date(b).getTime() - new Date(a).getTime()) // Sort dates in descending order
+            .filter((date) => {
+              if (!isAdmin) {
+                const todayStart = new Date();
+                todayStart.setHours(0, 0, 0, 0); // Set to midnight (start of today)
+
+                // Convert the 'date' key (string) to a Date object
+                const taskDate = new Date(date);
+                taskDate.setHours(0, 0, 0, 0); // Strip time part from the taskDate
+
+                // Only return dates before today if isAdmin is true
+                return taskDate < todayStart;
+              }
+              return true; // If not admin, return all dates
+            })
+            .map((date, index) => (
+              <div key={index}>
+                <div className="collapse collapse-arrow bg-black">
+                  <input type="radio" name="my-accordion-1" />
+                  <div className="collapse-title text-xl font-medium text-white">
+                    {format(new Date(date), "EEEE, do MMM")}
+                  </div>
+                  <div className="collapse-content">
+                    {groupedTasks[date]
+                      .sort(
+                        (a, b) =>
+                          new Date(b.created_at).getTime() -
+                          new Date(a.created_at).getTime()
+                      ) // Sort tasks within each date in descending order
+                      .map((task, index) => (
+                        <li
+                          key={index}
+                          className={`bg-gray-700 p-4 mb-4 w-full rounded-2xl flex items-center transition-all gap-4 ${task.is_complete
+                            ? "line-through bg-green-300/40 text-black/50"
+                            : ""
                             }`}
                         >
-                          {task.is_complete ? (
-                            <Icon
-                              icon="mdi:check"
-                              className="text-sm text-white"
-                            />
-                          ) : (
-                            <Icon
-                              icon="mdi:close"
-                              className="text-sm text-white"
-                            />
-                          )}
-                        </div>
-                        <h1 className="text-white">{task.name}</h1>
-                      </li>
-                    ))}
+                          <div
+                            className={`rounded-full flex justify-center items-center w-6 aspect-square ${task.is_complete ? "bg-green-500" : "bg-primary"
+                              }`}
+                          >
+                            {task.is_complete ? (
+                              <Icon
+                                icon="mdi:check"
+                                className="text-sm text-white"
+                              />
+                            ) : (
+                              <Icon
+                                icon="mdi:close"
+                                className="text-sm text-white"
+                              />
+                            )}
+                          </div>
+                          <h1 className="text-white">{task.name}</h1>
+                        </li>
+                      ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          )) : <h1>No small wins yet...</h1>}
+            ))
+        ) : (
+          <h1>No small wins yet...</h1>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default History;
