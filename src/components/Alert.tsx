dd3d4664d2febb1f6352da09f0031e4c1cd6a18e
Jack Gong <jackgong48@gmail.com>
2025-08-21 09:25:53 +1200

    fixed: strooke line join

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -22,7 +22,6 @@ const Alert = ({ error, status, success }: Props) => {
           >
             <path
               strokeLinecap="round"
-              strokeLineJoin="round"
               strokeWidth="2"
               d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
             />
