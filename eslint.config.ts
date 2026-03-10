741193313c1d2eb7b00de09fb9013263651301ba
Jack Gong <jackgong48@gmail.com>
2026-03-10 15:56:45 +0900

    idk

--- eslint.config.ts
+++ eslint.config.ts
@@ -24,8 +24,8 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
-        Request: "readonly",
-        Response: "readonly",
+        Request: "writeable",
+        Response: "writeable",
       },
     },
   }
